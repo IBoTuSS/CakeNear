60db1f012041fa6e402df96bf60cc520e0db79b4
IBoTuS <romanmiakotaa@gmail.com>
2025-07-22 23:15:49 +0300

    init

--- src/main/java/dev/cakestudio/cakenear/near/LuckPermsManager.java
+++ src/main/java/dev/cakestudio/cakenear/near/LuckPermsManager.java
@@ -1,22 +1,19 @@
 package dev.cakestudio.cakenear.near;
 
 import dev.cakestudio.cakenear.config.ConfigManager;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import net.luckperms.api.LuckPerms;
 import net.luckperms.api.model.user.User;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 
-/**
- * Класс-менеджер для взаимодействия с LuckPerms.
- * Отвечает за получение информации об игроках из LP.
- */
 @RequiredArgsConstructor
 public class LuckPermsManager {
 
     private final LuckPerms luckPerms;
 
-    private String getPlayerPrimaryGroup(Player player) {
+    private @NonNull String getPlayerPrimaryGroup(@NonNull Player player) {
         User user = luckPerms.getUserManager().getUser(player.getUniqueId());
         if (user != null) {
             return user.getPrimaryGroup();
@@ -30,7 +27,7 @@ public class LuckPermsManager {
 
         String groupPath = "near-distance.groups." + group;
 
-        return config.getInt(groupPath, config.getInt("near-distance.default", 100));
+        return config.getInt(groupPath, config.getInt("near-distance.default"));
     }
 }
 
